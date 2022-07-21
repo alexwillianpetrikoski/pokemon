80d5fc97968aa8ffef5e771cd26bbe8eb37835a0
Alex Petrikoski <alexwillian.petrikoski@gmail.com>
2022-07-22 00:44:50 +0100

    Mudei value do Input e iniciei o JS

---
+++ script.js
@@ -0,0 +1,10 @@
+var formulario = document.querySelector('form')
+
+formulario.addEventListener('submit', function(e){
+
+    e.preventDefault() //Bloqueia refresh da pagina
+
+    let urlForm = 'https://pokeapi.co/api/v2/pokemon/'; //URL da pesquisa 
+
+    let nome = document.getElementById('name')//Valor do input name
+});
