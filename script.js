52eabbf0a91f81d70a4a3471e198c602c8e55b02
Alex Petrikoski <alexwillian.petrikoski@gmail.com>
2022-07-22 22:01:34 +0100

    projeto finalizado

--- script.js
+++ script.js
@@ -7,4 +7,35 @@ formulario.addEventListener('submit', function(e){
     let urlForm = 'https://pokeapi.co/api/v2/pokemon/'; //URL da pesquisa 
 
     let nome = document.getElementById('name')//Valor do input name
+   
+    urlForm = urlForm + this.name.value//Concatena url com inputname
+
+    urlForm = urlForm.toLocaleLowerCase()
+
+    let resposta = document.getElementById('content')//ID content
+
+    let imagem = document.getElementById('imgPokemon')//Id imagem
+
+    let html = ''//resposta em HTML
+
+    fetch(urlForm)
+         .then(resposta => resposta.json())
+         .then(function (data) {
+            console.log(data)
+            html = 'Nome: ' + maiuscula (data.name) + '<br>'
+            html = html + 'Tipo: ' + maiuscula (data.types[0].type.name)
+            resposta.innerHTML = html
+
+            imagem.innerHTML = "<img src='" + data.sprites.front_default + "'><img src='" + data.sprites.back_default +"'>"
+         })
+         .catch(function(err){
+            console.log(err)
+         })
+
+    
 });
+
+function maiuscula(val){
+    return val[0].toUpperCase() + val.substr(1)
+}
+
